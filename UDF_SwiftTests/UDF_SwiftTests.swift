90f2054c157f666958b5bb9172c50039d2bb08aa
Grigory Avdyushin <avdyushin.g@gmail.com>
2017-07-11 09:54:18 +0200

    Template project added

---
+++ UDF_SwiftTests/UDF_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  UDF_SwiftTests.swift
+//  UDF_SwiftTests
+//
+//  Created by Grigory Avdyushin on 29/06/2017.
+//  Copyright © 2017 Grigory Avdyushin. All rights reserved.
+//
+
+import XCTest
+@testable import UDF_Swift
+
+class UDF_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
